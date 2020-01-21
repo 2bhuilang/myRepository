6e94e93c3b6f86e787402ca8c3c5989cdd4b1764
2bhuilang <88584058@qq.com>
2020-01-21 15:51:58 +0800

    111

--- src/com/greatcattle/start/ApplicationRoot.java
+++ src/com/greatcattle/start/ApplicationRoot.java
@@ -20,7 +20,9 @@ public class ApplicationRoot  extends Application{
 																									// 和
 		Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("com/greatcattle/view/main/mainframe.fxml"));// getClass().getClassLoader().getResource() // getClass().getResource()的区别，在不在根目录下
 		primaryStage.setTitle("窗口测试--zx");
-		primaryStage.setScene(new Scene(root, 1000, 600));
+		Scene scene = new Scene(root, 1000, 600);
+		primaryStage.setScene(scene);
+		//scene.getStylesheets().add(getClass().getClassLoader().getResource("com/greatcattle/view/css/base/app.css").toExternalForm());  
 		primaryStage.setResizable(true);// 窗体缩放（默认为true）   sdfgsdsdfgsdgf33
 		primaryStage.show();  
 	}
