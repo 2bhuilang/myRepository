94711518e5f17c8db2773b8a7d69f2b8f97ec8a4
2bhuilang <88584058@qq.com>
2020-01-29 18:56:41 +0800

    ffff

---
+++ src/com/greatcattle/business/bo/StaffBO.java
@@ -0,0 +1,32 @@
+package com.greatcattle.business.bo;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.greatcattle.dataservice.common.SqlMapExe;
+import com.greatcattle.dto.vo.StaffInfoVO;
+import com.greatcattle.utils.AppBeanUtil;
+
+/**
+ * 
+ * @author zhang.xian
+ *
+ */
+@Service
+public class StaffBO {
+	
+	
+	public StaffInfoVO getStaffInfo(String staffCode) {
+		
+		String sql = "";
+		
+		Map data = SqlMapExe.getInstance().queryForMapBySql(sql, new String[] {staffCode});
+		 
+		
+		return AppBeanUtil.queryResultMapToBean(data, StaffInfoVO.class);
+	}
+	
+	
+	
+}
