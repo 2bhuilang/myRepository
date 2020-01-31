cb4f6aaa1bb91323a1f7db72de1a879738a9db56
2bhuilang <88584058@qq.com>
2020-01-31 11:20:27 +0800

    sys

--- src/com/greatcattle/service/staff/StaffService.java
+++ src/com/greatcattle/service/staff/StaffService.java
@@ -8,14 +8,16 @@ import com.greatcattle.business.bo.StaffBO;
 import com.greatcattle.dto.view.LogInVO;
 import com.greatcattle.dto.vo.StaffInfoVO;
 import com.greatcattle.freamwork.utils.SpringUtils;
+import com.greatcattle.system.ctx.AppSysContext;
+import com.greatcattle.system.ctx.ContextFactory;
+import com.greatcattle.system.ctx.UserContext;
 import com.greatcattle.utils.StrTools;
 
 
 @Service
 public class StaffService {
 	
-	
-	
+	 
 	/**
 	 * 验证登录
 	 * @param loginVo
@@ -28,6 +30,9 @@ public class StaffService {
 		if(StrTools.isEqual(staffInfoVo.getPassWord(), loginVo.getPassword())) {
 			throw new RuntimeException("密码验证失败");
 		}
+		
+		UserContext userContext = ContextFactory.createUserContext(staffInfoVo);
+		AppSysContext.getSysCtx().setCurrentUserContext( userContext);
 		
 		return null;
 	}
