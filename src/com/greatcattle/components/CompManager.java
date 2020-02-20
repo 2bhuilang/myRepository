0c87ed58c4b0e0d63b178a9e53303ebfc1dd845d
2bhuilang <88584058@qq.com>
2020-02-20 22:53:25 +0800

    test

--- src/com/greatcattle/components/CompManager.java
+++ src/com/greatcattle/components/CompManager.java
@@ -2,6 +2,7 @@ package com.greatcattle.components;
 
 import com.greatcattle.components.drivers.camera.impl.stand.StandardCameraImpl;
 import com.greatcattle.components.drivers.weighbridge.impl.stand.StandardWeighBridgeImpl;
+import java.awt.Panel;
 
 /**
  * 设备管理
@@ -12,18 +13,48 @@ import com.greatcattle.components.drivers.weighbridge.impl.stand.StandardWeighBr
  */
 public class CompManager {
 	
-	
+        private CompManager(){
+            
+        }
+        private static CompManager compManager = null;
+        public static  CompManager initCompManager(java.awt.Panel panelRealplay){
+            if(compManager!=null){
+                return compManager ;
+             }
+            compManager =  new CompManager();
+            compManager.panelRealplay = panelRealplay;
+            compManager.initComps();
+            return compManager;
+        }
+        
+        public CompManager getCompManager(){
+            if(compManager!=null){
+                return compManager ;
+             }
+            return  null ;
+        }
+        
+        
+         
+	private java.awt.Panel panelRealplay;
 	/**
 	 * 初始化所有设备
 	 */
 	public void initComps() {
 		
 		//设置摄像头设备
-		CompRegCenter.getCompRegCenter().regCameraApi(new StandardCameraImpl());
+		CompRegCenter.getCompRegCenter().regCameraApi(new StandardCameraImpl(panelRealplay));
 		// 注册地磅设备
 		CompRegCenter.getCompRegCenter().regWeighBridgeApi( new StandardWeighBridgeImpl());
 		 
-	}
-	
+	} 
+
+        public Panel getPanelRealplay() {
+            return panelRealplay;
+        }
+
+        public void setPanelRealplay(Panel panelRealplay) {
+            this.panelRealplay = panelRealplay;
+        }
 	
 }
